fe7b808427ad0f275e4643825f442c79fadc8e55
isaiahn718 <55455558+isaiahn718@users.noreply.github.com>
2019-12-11 16:13:10 -0500

    third

--- pset5-master/js/shapes.js
+++ pset5-master/js/shapes.js
@@ -87,7 +87,25 @@ ctx.strokeRect(x, y, width, height);
  */
 
 const drawColoredRectangle = function() {
-    // write your exercise 3 code here
+  const canvas = document.getElementById('student-canvas-3');
+const ctx = canvas.getContext('2d');
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+do {
+ var color = (prompt("Color: "))
+ if (color == null) {
+   break;
+ }
+ color = String(color)
+ var color_case = color.toLowerCase()
+ if (color_case != "green" && color_case != "black" && color_case != "blue" && color_case != "orange" && color_case != "purple" && color_case != "red" && color_case != "yellow") {
+   alert(color + " is not a supported color.")
+ }
+} while (color_case != "green" && color_case != "black" && color_case != "blue" && color_case != "orange" && color_case != "purple" && color_case != "red" && color_case != "yellow")
+
+if (color != null) {
+ ctx.fillStyle = color_case;
+ ctx.fillRect(10, 10, 100, 50);
+}
 };
 
 /*
